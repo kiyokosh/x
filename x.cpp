b5ec50f1ba3d7cb117f1a0e92923f1555d55886f
kiyokosh <145582767+kiyokosh@users.noreply.github.com>
2023-09-20 17:52:54 +0300

    Add files via upload

---
+++ x.cpp
@@ -0,0 +1,49 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	
+	vector<vector<int>> b(3, vector<int> (3, 0));
+	cout << "- - - -"<<endl;
+	for (int i = 0; i < 3; i++)
+	{
+	
+		for (int a = 0; a < 3; a++)
+		{
+			cout << "|"<< b[i][a];
+			
+		}
+		cout << "|";
+		cout << endl;
+		
+		for (int a = 0; a < 3; a++)
+		{
+
+			cout << "- ";
+
+		}
+		cout << "- ";
+		cout << endl;
+
+	
+	
+		
+
+	}
+
+
+	    //cout << "   |   |  " << endl;
+		//cout << "___|___|___" << endl;
+		//cout << "   |   |  " << endl;
+		//cout << "___|___|___" << endl;
+		//cout << "   |   |  " << endl;
+        //cout << "   |   |  " << endl;
+	
+}
+
+
